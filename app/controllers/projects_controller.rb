b92e2f71734b693ed8c3efea38428d765233552b
Patrick Santos <patrick.sg.santos@gmail.com>
2023-04-17 10:16:25 -0300

    edit form not working properly yet due to multiples lines in detail images

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    raise
   end
 
   def new
@@ -32,13 +33,39 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    @project
   end
 
   def update
-    if @project.update(project_params)
+    @project.assign_attributes(project_params.except(:detail_images_row_1, :detail_images_row_2, :detail_images_row_3, :detail_images_row_4, :detail_images_row_5, :detail_images_row_6, :detail_images_row_7, :detail_images_row_8, :detail_images_row_9, :detail_images_row_10))
+
+    (1..10).each do |row_number|
+      row_images_param = "detail_images_row_#{row_number}".to_sym
+      existing_images = @project.detail_images.includes(:blob).where("metadata ->> 'row' = ?", row_number.to_s)
+
+      # Remove existing images for the current row
+      if project_params[row_images_param].nil?
+        existing_images.each do |image|
+          @project.detail_images.destroy(image)
+        end
+      else
+        # Add new images for the current row
+        project_params[row_images_param].each do |image|
+          if image.present?
+            @project.detail_images.attach(io: image.tempfile, filename: image.original_filename, content_type: image.content_type, metadata: { row: row_number })
+          end
+        end
+        # Remove the images that are no longer included in the params
+        (existing_images - @project.detail_images.where("metadata ->> 'row' = ?", row_number.to_s)).each do |image|
+          @project.detail_images.destroy(image)
+        end
+      end
+    end
+
+    if @project.save
       redirect_to @project, notice: 'Project was successfully updated.'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
