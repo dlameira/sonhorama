11bc06dda18ca9e10c3df3b29210e32d5076f82d
Patrick Santos <patrick.sg.santos@gmail.com>
2023-04-17 18:49:48 -0300

    video working on the banner

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_params.except(:detail_images_row_1, :detail_images_row_2, :detail_images_row_3, :detail_images_row_4, :detail_images_row_5, :detail_images_row_6, :detail_images_row_7, :detail_images_row_8, :detail_images_row_9, :detail_images_row_10))
+    @project = Project.new(project_params.except(:detail_images_row_1, :detail_images_row_2, :detail_images_row_3, :detail_images_row_4, :detail_images_row_5, :detail_images_row_6, :detail_images_row_7, :detail_images_row_8, :detail_images_row_9, :detail_images_row_10, :detail_images_row_11, :detail_images_row_12, :detail_images_row_13, :detail_images_row_14, :detail_images_row_15))
 
     (1..15).each do |row_number|
       row_images_param = "detail_images_row_#{row_number}".to_sym
@@ -37,34 +37,38 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    @project.assign_attributes(project_params.except(:detail_images_row_1, :detail_images_row_2, :detail_images_row_3, :detail_images_row_4, :detail_images_row_5, :detail_images_row_6, :detail_images_row_7, :detail_images_row_8, :detail_images_row_9, :detail_images_row_10))
+    # @project.assign_attributes(project_params.except(:detail_images_row_1, :detail_images_row_2, :detail_images_row_3, :detail_images_row_4, :detail_images_row_5, :detail_images_row_6, :detail_images_row_7, :detail_images_row_8, :detail_images_row_9, :detail_images_row_10, :detail_images_row_11, :detail_images_row_12, :detail_images_row_13, :detail_images_row_14, :detail_images_row_15))
 
-      (1..15).each do |row_number|
-      row_images_param = "detail_images_row_#{row_number}".to_sym
-      row_images = @project.detail_images.first(row_number)
+    #   (1..15).each do |row_number|
+    #   row_images_param = "detail_images_row_#{row_number}".to_sym
+    #   row_images = @project.detail_images.first(row_number)
 
-      if project_params[row_images_param].nil?
-        # Remove existing images for the current row
-        row_images.purge
-      else
-        # Remove existing images for the current row
-        row_images.each(&:purge)
+    #   if project_params[row_images_param].nil?
+    #     # Remove existing images for the current row
+    #     row_images.purge
+    #   else
+    #     # Remove existing images for the current row
+    #     row_images.each(&:purge)
 
-        # Add new images for the current row
-        project_params[row_images_param].each do |image|
-          if image.present?
-            @project.detail_images.attach(io: image.tempfile, filename: image.original_filename, content_type: image.content_type, metadata: { row: row_number })
-          end
-        end
-      end
+    #     # Add new images for the current row
+    #     project_params[row_images_param].each do |image|
+    #       if image.present?
+    #         @project.detail_images.attach(io: image.tempfile, filename: image.original_filename, content_type: image.content_type, metadata: { row: row_number })
+    #       end
+    #     end
+    #   end
 
-    end
-
-    if @project.save
+    if @project.update(project_params)
       redirect_to @project, notice: 'Project was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
+
+    # if @project.save
+    #   redirect_to @project, notice: 'Project was successfully updated.'
+    # else
+    #   render :edit, status: :unprocessable_entity
+    # end
   end
 
   private
@@ -74,7 +78,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:title, :subheading, :description, :callout, :collaborators, :client, :date, :thumbnail, :banner, :content,
+    params.require(:project).permit(:title, :subheading, :description, :callout, :collaborators, :client, :date, :thumbnail, :banner, :content, :listed,
                                       detail_images_row_1: [], detail_images_row_2: [], detail_images_row_3: [],
                                       detail_images_row_4: [], detail_images_row_5: [], detail_images_row_6: [],
                                       detail_images_row_7: [], detail_images_row_8: [], detail_images_row_9: [],
